948d7f99d976ef478183f182a619c0fb87be2803
john smith <a8961713@gmail.com>
2025-07-28 14:19:52 +0300

    Add a specific zephyr version (currently 4.2.0), circumvent blob fetch problem

--- download_zephyr.sh
+++ download_zephyr.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
-west init zephyrproject
+west init --mr v4.2.0 zephyrproject
 cd zephyrproject
-west update 
+west update
+
+# Add logging functions because error 'AttributeError: 'Blobs' object has no attribute 'dbg'' (for example) is received
+sed -i '/^class /a\ \ \ \ def dbg(self, msg):\n\ \ \ \ \ \ \ \ print(msg)' /zephyrproject/zephyr/scripts/west_commands/blobs.py 
+sed -i '/^class /a\ \ \ \ def inf(self, msg):\n\ \ \ \ \ \ \ \ print(msg)' /zephyrproject/zephyr/scripts/west_commands/blobs.py 
+sed -i '/^class /a\ \ \ \ def wrn(self, msg):\n\ \ \ \ \ \ \ \ print(msg)' /zephyrproject/zephyr/scripts/west_commands/blobs.py 
+sed -i '/^class /a\ \ \ \ def err(self, msg):\n\ \ \ \ \ \ \ \ print(msg)' /zephyrproject/zephyr/scripts/west_commands/blobs.py 
 west blobs fetch hal_espressif
 west blobs fetch hal_stm32
+
 find . -name \.git|xargs rm -rf
+
 cd ..
 mkdir -p /artifacts/pip/requirements1
 cp ./zephyrproject/zephyr/scripts/requirements* /artifacts/pip/requirements1/
-
 mkdir -p /artifacts/pip/requirements2
 cp ./zephyrproject/bootloader/mcuboot/scripts/requirements* /artifacts/pip/requirements2/
 
